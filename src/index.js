fa974c0c79187b699b431a8aba6b1ff2928549d0
wlwl0219 <wlwl0219@gmail.com>
2021-05-27 18:26:47 +0900

    Update routers controllers

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import express from 'express';
 import morgan from 'morgan';
 
+import globalRouter from './routers/globalRouter';
+import videoRouter from './routers/videoRouter';
+import userRouter from './routers/userRouter';
+
 const app = express();
 const logger = morgan('dev');
 
-const handleHome = (req, res) => {
-  return res.send('I still love you.');
-};
-const handleLogin = (req, res) => {
-  return res.send('Login here.');
-};
-
 app.use(logger);
-app.get('/', handleHome);
-app.get('/login', handleLogin);
+app.use('/', globalRouter);
+app.use('/videos', videoRouter);
+app.use('/users', userRouter);
 
 const PORT = 4000;
 const handleListening = () =>
