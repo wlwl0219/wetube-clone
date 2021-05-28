9d74db97fcab1f05ee7a9cd19aff921dc985418f
wlwl0219 <wlwl0219@gmail.com>
2021-05-28 11:11:48 +0900

    Update Router url parameters

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { edit, remove } from '../controllers/userController';
+import { see, edit, remove, logout } from '../controllers/userController';
 
 const userRouter = express.Router();
 
 userRouter.get('/edit', edit);
 userRouter.get('/remove', remove);
+userRouter.get('/logout', logout);
+userRouter.get(':id', see);
 
 export default userRouter;
