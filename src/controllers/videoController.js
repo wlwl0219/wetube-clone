4554cf9decb70aa188921ef09124074c598d8df7
wlwl0219 <wlwl0219@gmail.com>
2021-06-03 01:12:23 +0900

    Add template, mixin

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,37 @@
+const fakeUser = {
+  username: 'Nicolas',
+  loggedIn: true,
+};
+
+const videos = [
+  {
+    title: 'First Video',
+    rating: 5,
+    comments: 2,
+    createdAt: '2 minutes ago',
+    views: 59,
+    id: 1,
+  },
+  {
+    title: 'Video #2',
+    rating: 5,
+    comments: 2,
+    createdAt: '2 minutes ago',
+    views: 59,
+    id: 1,
+  },
+  {
+    title: 'Third Video',
+    rating: 5,
+    comments: 2,
+    createdAt: '2 minutes ago',
+    views: 59,
+    id: 1,
+  },
+];
+
 export const trending = (req, res) =>
-  res.render('home', { pageTitle: '되니?' });
+  res.render('home', { pageTitle: '되니?', fakeUser, videos });
 export const search = (req, res) => res.send('Search');
 export const see = (req, res) => res.send('Watch');
 export const edit = (req, res) => res.send('Edit');
